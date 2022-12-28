6461e3ea6ae691a309d3a3ad2b824de70e9a2ea5
Juan Carlos Lujan <juanclujan.92@gmail.com>
2022-12-28 10:11:40 -0300

    Add files via upload

---
+++ FizzBuzz.js
@@ -0,0 +1,14 @@
+for (let i = 0; i < 100; i++) {
+    var output = "";
+    if (i % 3 == 0) {
+        output += "Fizz";
+    }
+    if (i % 5 == 0) {
+        output += "Buzz";
+    }
+    if (output == "") {
+        output == i;
+    }
+    
+    console.log(output);
+}
